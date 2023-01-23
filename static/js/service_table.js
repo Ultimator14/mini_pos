2951d3b37cb141359c2c409c5ec62c574022e2a9
Jan Breig <git@pygos.space>
2023-01-23 19:10:57 +0100

    Add overview in service to view selected orders

--- static/js/service_table.js
+++ static/js/service_table.js
@@ -1,3 +1,77 @@
+function compare_ids(a, b) {
+    if (a.id < b.id) {
+        return -1;
+    }
+    if (a.id > b.id) {
+        return 1;
+    }
+    return 0;
+}
+
+function updateValues() {
+    let amounts = Array.from(document.getElementsByClassName("amount-box"));
+    let amounts2 = Array.from(document.getElementsByClassName("amount2-text"));
+    let comments = Array.from(document.getElementsByClassName("comment-box"));
+    let costs = Array.from(document.getElementsByClassName("cost-text"));
+    let names = Array.from(document.getElementsByClassName("template-product-name"));
+    let prices = Array.from(document.getElementsByClassName("price-text"));
+    let rows = Array.from(document.getElementsByClassName("template-product-row"));
+
+    amounts.sort(compare_ids);
+    amounts2.sort(compare_ids);
+    comments.sort(compare_ids);
+    costs.sort(compare_ids);
+    names.sort(compare_ids);
+    prices.sort(compare_ids);
+    rows.sort(compare_ids);
+
+    let sum = 0;
+    let overview = document.getElementById("overview");
+
+    overview.innerHTML = "";  //clear children of overview list
+
+    for(let i=0; i<prices.length; i++) {
+        //Extract values
+        let amount = amounts[i].value;
+        let comment = comments[i].value;
+        let name = names[i].innerHTML;
+        let price = prices[i].innerHTML;
+
+        //Compute per product cose and accumulation
+        let current_cost = parseFloat(price) * parseFloat(amount);
+        sum += current_cost;
+
+        //Set cost field
+        costs[i].innerHTML = current_cost.toFixed(2).toString();
+
+        //Sync second amount field
+        amounts2[i].innerHTML = amount;
+
+        //Colorize table row if amount is > 0
+        if (parseFloat(amount) > 0) {
+            rows[i].classList.add("colorized-row");
+
+            //Generate overview
+            let entry = document.createElement('li');
+            let entry_text = amount + "x " + name
+
+            if (comment !== "") {
+                entry_text += " (" + comment + ")"
+            }
+
+            entry.appendChild(document.createTextNode(entry_text));
+            overview.appendChild(entry);
+        }
+        else {
+            rows[i].classList.remove("colorized-row");
+        }
+    }
+
+    //Recompute total value
+    let total = document.getElementById("total-cost");
+    total.innerHTML = sum.toFixed(2).toString();
+}
+
 function showPopup(pid) {
     /*Display popup*/
     document.getElementById("customize-popup-" + pid).style.display = "block";
@@ -17,60 +91,8 @@ function hidePopup(pid) {
 
     /*Hide popup*/
     document.getElementById("customize-popup-" + pid).style.display = "none";
-}
 
-function compare_ids(a, b) {
-    if (a.id < b.id) {
-        return -1;
-    }
-    if (a.id > b.id) {
-        return 1;
-    }
-    return 0;
-}
-
-function updateValues() {
-    let prices = Array.from(document.getElementsByClassName("price-text"));
-    let amounts = Array.from(document.getElementsByClassName("amount-box"));
-    let costs = Array.from(document.getElementsByClassName("cost-text"));
-    let amounts2 = Array.from(document.getElementsByClassName("amount2-text"));
-    let rows = Array.from(document.getElementsByClassName("template-product-row"));
-
-    prices.sort(compare_ids);
-    amounts.sort(compare_ids);
-    costs.sort(compare_ids);
-    amounts2.sort(compare_ids);
-    rows.sort(compare_ids);
-
-    let sum = 0;
-
-    for(let i=0; i<prices.length; i++) {
-        let price = parseFloat(prices[i].innerHTML);
-        let amount = parseFloat(amounts[i].value);
-
-        //Compute per product cose and accumulation
-        let current_cost = price * amount;
-        sum += current_cost;
-
-        //Set cost field
-        costs[i].innerHTML = current_cost.toFixed(2).toString();
-
-        //Sync second amount field
-        amounts2[i].innerHTML = amount.toString();
-
-        //Colorize table row if amount is > 0
-        let row = rows[i];
-        if (amount > 0) {
-            row.classList.add("colorized-row");
-        }
-        else {
-            row.classList.remove("colorized-row");
-        }
-    }
-
-    //Recompute total value
-    let total = document.getElementById("total-cost");
-    total.innerHTML = sum.toFixed(2).toString();
+    updateValues()
 }
 
 function modifyAmount(pid, value) {
