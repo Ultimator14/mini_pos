cfc177b37b1f86fe74b5755c261b6e04d6936536
Jan Breig <git@pygos.space>
2023-07-25 15:50:41 +0200

    Add indication in bar whether server is down

--- static/js/bar.js
+++ static/js/bar.js
@@ -1,6 +1,19 @@
 let myTimer = setInterval(updateBarBody, 3000);
 
 async function updateBarBody() {
-    const response = await fetch("/fetch/bar");
-    document.getElementsByTagName('body')[0].innerHTML = await response.text();
+    let server_status_div = document.getElementById("server-status");
+
+    try {
+        const response = await fetch("/fetch/bar");
+        document.getElementsByTagName('body')[0].innerHTML = await response.text();
+
+        server_status_div.classList.remove("server-status-down");
+        server_status_div.classList.add("server-status-up");
+        server_status_div.innerHTML = "Server is up";
+
+    } catch (error) {
+        server_status_div.classList.remove("server-status-up");
+        server_status_div.classList.add("server-status-down");
+        server_status_div.innerHTML = "Server is down";
+    }
 }
