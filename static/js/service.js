e8a74dceb6b931d0eda127fdc8e714f0467634b7
Jan Breig <git@pygos.space>
2023-07-25 15:01:36 +0200

    Avoid needless class changes in service

--- static/js/service.js
+++ static/js/service.js
@@ -3,20 +3,28 @@ let myTimer = setInterval(updateActiveTables, 1000);
 async function updateActiveTables() {
     //Request new tables
     const response = await fetch("/fetch/service");
-    const tables_new = await response.json();
+    const tables_new_ids = await response.json();
 
-    //Reset all tables
+    //Get table elements
     let tables_cur = Array.from(document.getElementsByClassName("active-order"));
+    let tables_new = [];
+    for(let i=0; i<tables_new_ids.length; i++) {
+        tables_new.push(document.getElementById(tables_new_ids[i]))
+    }
 
-    for(let i=0; i<tables_cur.length; i++) {
-        tables_cur[i].classList.remove("active-order");
-        tables_cur[i].classList.add("no-order");
+    //Get tables updates
+    let added_tables = tables_new.filter(x => ! tables_cur.includes(x));
+    let removed_tables = tables_cur.filter(x => ! tables_new.includes(x));
+
+    //Reset removed tables
+    for(let i=0; i<removed_tables.length; i++) {
+        removed_tables[i].classList.remove("active-order");
+        removed_tables[i].classList.add("no-order");
     }
 
     //Set active tables to active
-    for(let i=0; i<tables_new.length; i++) {
-        let table = document.getElementById(tables_new[i]);
-        table.classList.remove("no-order");
-        table.classList.add("active-order");
+    for(let i=0; i<added_tables.length; i++) {
+        added_tables.classList.remove("no-order");
+        added_tables.classList.add("active-order");
     }
 }
