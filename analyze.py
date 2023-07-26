f7cbe0470a864022cc17e9f79874b61ed2143a1b
Jan Breig <git@pygos.space>
2023-07-26 19:12:08 +0200

    Adapt analysis script

--- analyze.py
+++ analyze.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3.10
 
 import app
+from app import db
 
-app.log_info("----------------------------------------")
-app.log_info("Starting analysis")
-app.log_info("----------------------------------------")
-app.log_info(f"Active orders: {str(len(app.orders))}")
-app.log_info(f"Completed orders: {str(len(app.completed_orders))}")
-app.log_info("----------------------------------------")
+with app.app.app_context():
+    def get_completed_orders() -> list[app.Order]:
+        return list(db.session.execute(
+            db.select(app.Order)
+            .filter(app.Order.completed_at != None)  # this must be != and not 'is not'
+        ).scalars())
+
+    orders = app.get_open_orders()
+    completed_orders = get_completed_orders()
+
+    app.log_info("----------------------------------------")
+    app.log_info("Starting analysis")
+    app.log_info("----------------------------------------")
+    app.log_info(f"Active orders: {str(len(orders))}")
+    app.log_info(f"Completed orders: {str(len(completed_orders))}")
+    app.log_info("----------------------------------------")
 
 
-products = {}
+    products: dict[str, int] = {}
 
-for order in app.completed_orders:
-    for product in order.products:
-        if product.name in products:
-            products[product.name] += product.amount
-        else:
-            products[product.name] = product.amount
+    for order in completed_orders:
+        for product in order.products:
+            if product.name in products:
+                products[product.name] += product.amount
+            else:
+                products[product.name] = product.amount
 
-
-app.log_info("Sold units")
-app.log_info("----------")
-for product in sorted(products, key=products.get, reverse=True):
-    app.log_info(f"{product}: {str(products[product])}")
-app.log_info("----------------------------------------")
+    app.log_info("Sold units")
+    app.log_info("----------")
+    for product in sorted(products, key=products.get, reverse=True):
+        app.log_info(f"{product}: {str(products[product])}")
+    app.log_info("----------------------------------------")
