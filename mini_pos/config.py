ecce1d27ff6282ced73114d935e5a78d64308c8a
Jan Breig <git@pygos.space>
2023-08-04 18:18:09 +0200

    Check config file for correct syntax, types, and mandatory values

--- mini_pos/config.py
+++ mini_pos/config.py
@@ -4,7 +4,7 @@ import json
 import logging
 import os
 import sys
-from typing import Any
+from typing import Any, get_args, get_origin
 
 from flask import current_app as app
 
@@ -13,22 +13,127 @@ TablesGridTupleT = tuple[bool, int | None, int | None, str | None]
 TablesGridT = list[list[TablesGridTupleT | None]]
 
 
+TYPING_DICT = {
+    "product": {
+        "available": list[tuple[str, float, int]],
+        "categories": list[tuple[int, str]],
+    },
+    "table": {
+        "size": tuple[int, int],
+        "names": list[tuple[int, int, int, int, str]],
+    },
+    "ui": {
+        "auto_close": bool,
+        "show_completed": int,
+        "show_category_names": bool,
+        "split_categories": bool,
+        "timeout": tuple[int, int],
+    },
+    "debug": bool,
+}
+
+MANDATORY_DICT = {
+    "product": {
+        "available": True,
+        "categories": True,
+    },
+    "table": {
+        "size": True,
+        "names": True,
+    },
+}
+
+
+def check_type(data: Any, expected_type: type, path: str) -> bool:
+    """Check if data matches a primitive expected type.
+    e.g. check if data has type str
+    """
+    if isinstance(data, expected_type):
+        return True
+
+    app.logger.critical("Invalid type for config option: %s", path)
+    return False
+
+
+def unpack_type(data: Any, expected_type: type, path: str) -> bool:
+    """Check if data matches a nested expected type.
+    e.g. check if data has type list[tuple[str,int]]
+    """
+    origin = get_origin(expected_type)
+
+    # ban dicts
+    if origin is dict:
+        err_msg = "Nested dictionary types are not supported for type checking config"
+        raise TypeError(err_msg)
+
+    # primitive data type, nothing to unpack
+    if origin is None:
+        return check_type(data, expected_type, path)
+
+    path = f"{path} -> {origin.__name__!s}"
+
+    # check parent type (JSON only has lists, python needs tuples)
+    safe_origin = list if origin is tuple else origin
+    if not check_type(data, safe_origin, path):
+        return False
+
+    # check child types (only if parent type is correct)
+    type_params = get_args(expected_type)
+
+    if origin is list:
+        return all(unpack_type(d, type_params[0], path) for d in data)
+
+    if origin is tuple:
+        if len(type_params) != len(data):
+            app.logger.critical("Config option has wrong length (must be %s): %s", len(type_params), data)
+            return False
+
+        return all(unpack_type(data[index], type_param, path) for index, type_param in enumerate(type_params))
+
+    return all(check_type(data[tp], type_params[tp], path) for tp in type_params)
+
+
+def check_config(data: Any, typing_dict: dict, mandatory_dict: dict | None, path="."):
+    """Check if data conforms to the nested datastructure typing_dict.
+    Mandatory keys must be present in mandatory_dict.
+    e.g. check if data matches {"mykey": list[tuple[str,int]]}
+    """
+    check_result = True
+
+    # Iterate over all supported config options
+    for key, expected_type in typing_dict.items():
+        subpath = f"{path} -> {key}"
+
+        if key in data:
+            # Key is present in config
+            if type(expected_type) is dict:
+                if check_type(data[key], dict, subpath):
+                    md = mandatory_dict.get(key) if type(mandatory_dict) is dict else None
+                    check_result = check_config(data[key], typing_dict[key], md, subpath) and check_result
+                else:
+                    check_result = False
+            else:
+                check_result = unpack_type(data[key], expected_type, subpath) and check_result
+        else:
+            # Key is missing from config
+            if mandatory_dict is not None and mandatory_dict.get(key):
+                app.logger.critical("Mandatory key missing from config: %s", subpath)
+                check_result = False
+            else:
+                app.logger.warning("Optional key missing from config: %s", subpath)
+
+    return check_result
+
+
 class ProductConfig:
-    procuct_type: type= list[list[tuple[str,str]]]
+    procuct_type: type = list[list[tuple[str, str]]]
 
     def __init__(self) -> None:
         self.available: AvailableProductsT = {}
         self.category_map: dict[int, str] = {}
 
-    def set_options(self, product: dict[str,Any]) -> None:
-        if (available_config := product.get("available")) is None:
-            app.logger.critical("prouct->available missing in config file")
-        elif len(available_config) == 0:
-            app.logger.critical("config option product->available must be of length >0")
-        elif not all(tuple(type(x) for x in y) == (str, float, int) for y in available_config):
-            app.logger.critical("config option product->available must be of type list(str, float, int)")
-        else:
-            self.available = dict(enumerate([tuple(product) for product in available_config], start=1))  # type: ignore
+    def set_options(self, product: dict[str, Any]) -> None:
+        self.available = dict(enumerate([tuple(product) for product in product["available"]], start=1))  # type: ignore
 
         if (categories_config := product.get("categories")) is None:
             app.logger.critical("prouct->categories missing in config file")
@@ -44,7 +149,7 @@ class TableConfig:
         self.grid: TablesGridT = []
         self.names: list[str]
 
-    def populate_grid(self, names: list[tuple[int,int,int,int,str]]) -> None:
+    def populate_grid(self, names: list[tuple[int, int, int, int, str]]) -> None:
         grid: TablesGridT = [[None for x in range(self.size[0])] for y in range(self.size[1])]
 
         # parse tables
@@ -64,25 +169,14 @@ class TableConfig:
 
         self.grid = grid
 
-    def set_options(self, table: dict[str,Any]) -> None:
-        if (size_config := table.get("size")) is None:
-            app.logger.critical("table->size missing in config file")
-        elif len(size_config) != 2:
-            app.logger.critical("config option table->size must be exactly of length 2")
-        elif tuple(type(x) for x in size_config) != (int, int):
-            app.logger.critical("config option table->size must be of type (int, int)")
-        else:
-            self.size = tuple(size_config)  # type: ignore
+    def set_options(self, table: dict[str, Any]) -> None:
+        self.size = tuple(table["size"])  # type: ignore
 
-        if (names_config := table.get("names")) is None:
-            app.logger.critical("table->names missing in config file")
-        elif len(names_config) == 0:
-            app.logger.critical("config option table->names must be of length >0")
-        else:
-            self.names = [name for _, _, _, _, name in names_config]
-            if len(set(self.names)) != len(self.names):
-                app.logger.critical("Duplicate table name found. Tables names must be unique")
-            self.populate_grid(names_config)
+        names_config = table["names"]
+        self.names = [name for _, _, _, _, name in names_config]
+        if len(set(self.names)) != len(self.names):
+            app.logger.critical("Duplicate table name found. Tables names must be unique")
+        self.populate_grid(names_config)
 
 
 class UIConfig:
@@ -105,51 +199,51 @@ class UIConfig:
 class MiniPOSConfig:
     def __init__(self) -> None:
         self.product: ProductConfig = ProductConfig()
-        self.ui: UIConfig = UIConfig()
         self.table: TableConfig = TableConfig()
-
-    def load_file(self, config_file: str) -> None:
-        if not os.path.isfile(config_file):
-            app.logger.critical("No config file found. Abort execution")
-
-        app.logger.info("Loading configuration...")
-
-        with open(config_file, encoding="utf-8") as afile:
-            try:
-                config_data = json.load(afile)
-                self.set_options(config_data)
-            except json.decoder.JSONDecodeError as e:
-                app.logger.critical("Broken configuration file: %s", repr(e))
+        self.ui: UIConfig = UIConfig()
 
     def set_options(self, config_data: dict) -> None:
         # debug setting also used for flask
-        app.config["DEBUG"] = config_data.get("debug", app.config["DEBUG"])
+        app.config["DEBUG"] = config_data.get("debug", app.config["DEBUG"])  # optional
 
-        if (product := config_data.get("product")) is None:
-            app.logger.critical("product section missing in config file.")
-        else:
-            self.product.set_options(product)
+        self.product.set_options(config_data["product"])  # required
+        self.table.set_options(config_data["table"])  # required
+        self.ui.set_options(config_data.get("ui", {}))  # optional
 
-        if (table := config_data.get("table")) is None:
-            app.logger.critical("table section missing in config file.")
-        else:
-            self.table.set_options(table)
 
-        if (ui := config_data.get("ui")) is None:
-            app.logger.warning("ui section is missing in config file. Using defaults.")
-        else:
-            self.ui.set_options(ui)
+def load_file(config_file: str) -> dict | None:
+    if not os.path.isfile(config_file):
+        app.logger.critical("No config file found.")
+        return None
+
+    app.logger.info("Loading configuration...")
+
+    with open(config_file, encoding="utf-8") as afile:
+        try:
+            return json.load(afile)
+        except json.decoder.JSONDecodeError as e:
+            app.logger.critical("Broken configuration file: %s", repr(e))
+            return None
 
 
 def init_config(app):
+    # Check if config file exists and has no json errors
+    if (config_data := load_file(app.config["CONFIG_FILE"])) is None:
+        sys.exit(1)
+
+    # Check if config conforms to correct structure
+    if not check_config(config_data, TYPING_DICT, MANDATORY_DICT):
+        sys.exit(1)
+
     # Get the crit log count handler to use
     crit_log_count_handler = next((x for x in app.logger.handlers if x.name == "CritLogCountHandler"), None)
-    assert(crit_log_count_handler is not None)  # defined in log.py, should not be None
+    assert crit_log_count_handler is not None  # defined in log.py, should not be None
 
+    # Initialize config object from json
     _ = crit_log_count_handler.count  # reset counter
 
     mp = MiniPOSConfig()
-    mp.load_file(app.config["CONFIG_FILE"])
+    mp.set_options(config_data)
     app.config["minipos"] = mp
 
     if crit_log_count_handler.count != 0:
