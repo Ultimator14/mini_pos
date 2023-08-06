ce2f328f399fecbb05a9f6b2dd20629aba7d75bc
Jan Breig <git@pygos.space>
2023-08-06 23:55:04 +0200

    Remove unnecessary test settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,3 @@
-import os
-import tempfile
-
 import pytest
 
 from mini_pos import create_app
@@ -9,14 +6,7 @@ from mini_pos.settings import TestConfig
 
 @pytest.fixture()
 def app():
-    db_fd, db_path = tempfile.mkstemp()
-
-    flask_app = create_app(config=TestConfig)
-
-    yield flask_app
-
-    os.close(db_fd)
-    os.unlink(db_path)
+    return create_app(config=TestConfig)
 
 
 @pytest.fixture()
