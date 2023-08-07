c8480f1d7bd45e2c5caf65b1c4d3425f5d621887
Jan Breig <git@pygos.space>
2023-08-07 02:34:53 +0200

    Don't trust user to submit correct order id for product
    This is an artifact before using sql where products were part of orders.
    In the current implementation, orders are determined by products.

--- mini_pos/routes/bar.py
+++ mini_pos/routes/bar.py
@@ -30,16 +30,10 @@ def bar_submit():
             handle_order_completed_event(int(order_id))
 
     elif "product-completed" in request.form:
-        if "order" in request.form:
-            if (
-                not (product_id := request.form["product-completed"]).isdigit()
-                or not (order_id := request.form["order"]).isdigit()
-            ):
-                app.logger.error("POST in /bar but filetype not convertible to integer")
-            else:
-                handle_product_completed_event(int(product_id), int(order_id))
+        if not (product_id := request.form["product-completed"]).isdigit():
+            app.logger.error("POST in /bar but filetype not convertible to integer")
         else:
-            app.logger.error("POST in /bar but missing order data for completed product")
+            handle_product_completed_event(int(product_id))
 
     else:
         app.logger.error("POST in /bar but neither order nor product specified")
@@ -51,27 +45,27 @@ def handle_order_completed_event(order_id: int) -> None:
     order = Order.get_order_by_id(order_id)
 
     if order is None:
-        app.logger.error("POST in /bar but no matching Order to complete")
+        app.logger.error("POST in /bar but no matching order to complete")
         return
 
     order.complete()
 
 
-def handle_product_completed_event(product_id: int, order_id: int) -> None:
-    order = Order.get_order_by_id(order_id)
-
-    if order is None:
-        app.logger.error("POST in /bar but no matching Order found")
-        return
-
+def handle_product_completed_event(product_id: int) -> None:
     product = Product.get_product_by_id(product_id)
 
     if product is None:
-        app.logger.error("POST in /bar but no matching Product for order found")
+        app.logger.error("POST in /bar but no matching product found")
         return
 
     product.complete()
+    order_id = product.order_id
 
-    if app.config["minipos"].ui.auto_close and len(Product.get_open_products_by_order_id(order.id)) == 0:
+    if app.config["minipos"].ui.auto_close and len(Product.get_open_products_by_order_id(order_id)) == 0:
         app.logger.info("Last Product completed. Attempting auto_close")
+
+        if (order := Order.get_order_by_id(order_id)) is None:
+            app.logger.error("POST in /bar but no matching order for product found")
+            return
+
         order.complete()
