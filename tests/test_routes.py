7ef67874613255adfb762ad99ea11a048808ca97
Jan Breig <git@pygos.space>
2024-01-12 14:12:56 +0100

    Update waiter name tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,20 +16,26 @@ def test_service_login(client):
     assert b"<title>Service Login</title>" in response.data
 
 
-def test_service_login_redirect(client):
-    response = client.get("/service")
+def test_service_trylogin(client):
+    response = client.get("/service/trylogin")
     assert response.status_code == 302
     assert b"/service/login" in response.data
 
 
-def test_service(client):
+def test_service_trylogin2(client):
     client.set_cookie("waiter", "")
+    response = client.get("/service/trylogin")
+    assert response.status_code == 302
+    assert b"/service" in response.data
+    assert b"/service/login" not in response.data
+
+
+def test_service(client):
     response = client.get("/service")
     assert b"<title>Service</title>" in response.data
 
 
 def test_service2(client):
-    client.set_cookie("waiter", "")
     response = client.get("/service/A1")
     assert b"<title>Service Table A1</title>" in response.data
 
