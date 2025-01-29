543e38aea5bd17f9ed83ef6e0dc47bc899c6df40
Jan Breig <git@pygos.space>
2025-01-29 15:37:57 +0100

    Rename name property to bar in fetch

--- mini_pos/routes/fetch.py
+++ mini_pos/routes/fetch.py
@@ -6,26 +6,26 @@ from mini_pos.models import Order
 fetch_bp = Blueprint("fetch", __name__, template_folder="templates")
 
 
-@fetch_bp.route("/bar/<name>", strict_slashes=False)
-def fetch_bar(name: str):
-    app.logger.debug("GET /fetch/bar/<name>")
+@fetch_bp.route("/bar/<bar>", strict_slashes=False)
+def fetch_bar(bar: str):
+    app.logger.debug("GET /fetch/bar/<bar>")
 
-    if app.config["minipos"].bars.get(name) is None:
-        app.logger.error("GET in /bar/%s with invalid bar. Using default bar. Skipping...", name)
+    if app.config["minipos"].bars.get(bar) is None:
+        app.logger.error("GET in /bar/%s with invalid bar. Using default bar. Skipping...", bar)
         return "Error! Bar not found"
 
     open_orders = Order.get_open_orders()
     return render_template(
         "bar_body.html",
-        orders=[o for o in open_orders if any(not p.completed for p in o.products_for_bar(name))],
+        orders=[o for o in open_orders if any(not p.completed for p in o.products_for_bar(bar))],
         partially_completed_orders=[
             o
             for o in open_orders
-            if all(p.completed for p in o.products_for_bar(name)) and len(o.products_for_bar(bar)) > 0
+            if all(p.completed for p in o.products_for_bar(bar)) and len(o.products_for_bar(bar)) > 0
         ],
-        completed_orders=[co for co in Order.get_last_completed_orders() if co.products_for_bar(name)],
+        completed_orders=[co for co in Order.get_last_completed_orders() if co.products_for_bar(bar)],
         show_completed=bool(app.config["minipos"].ui.bar.show_completed),
-        bar=name,
+        bar=bar,
     )
 
 
