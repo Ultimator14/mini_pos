4afc8bc897c0b6f125c61dcaa8a9a5017181abe7
Jan Breig <git@pygos.space>
2025-02-04 09:27:37 +0100

    Reformat service_table.js

--- mini_pos/static/js/service_table.js
+++ mini_pos/static/js/service_table.js
@@ -39,241 +39,241 @@ function updateValues() {
         let name = names[i].innerHTML;
         let price = prices[i].innerHTML;
 
-            //Catch negative values
-            if (amount < 0) {
-                amount = 0;
-                amounts[i].value = 0;
-            }
-
-            //Compute per product cose and accumulation
-            let current_cost = parseFloat(price) * parseFloat(amount);
-            sum += current_cost;
-
-            //Set cost field
-            costs[i].innerHTML = current_cost.toFixed(2).toString();
-
-            //Sync second amount field
-            amounts2[i].innerHTML = amount;
-
-            //Colorize table row if amount is > 0
-            if (parseFloat(amount) > 0) {
-                rows[i].classList.add("colorized-row");
-
-                //Generate overview
-                let entry = document.createElement('li');
-                let entry2 = document.createElement('li');
-
-                let entry_text = amount + "x " + name
-                entry_text += (comment !== "" ? " (" + comment + ")" : "")
-                let entry_text2 = current_cost.toFixed(2).toString() + "€"
-
-                entry.appendChild(document.createTextNode(entry_text));
-                entry2.appendChild(document.createTextNode(entry_text2));
-
-                overview.appendChild(entry);
-                overview.appendChild(entry2);
-            }
-            else {
-                rows[i].classList.remove("colorized-row");
-            }
+        //Catch negative values
+        if (amount < 0) {
+            amount = 0;
+            amounts[i].value = 0;
         }
 
-        //Recompute total value
-        let total = document.getElementById("total-cost");
-        total.innerHTML = sum.toFixed(2).toString();
-    }
+        //Compute per product cose and accumulation
+        let current_cost = parseFloat(price) * parseFloat(amount);
+        sum += current_cost;
 
-    function showPopup(pid) {
-        /*Display popup*/
-        document.getElementById("customize-popup-" + pid).style.display = "block";
-    }
+        //Set cost field
+        costs[i].innerHTML = current_cost.toFixed(2).toString();
 
-    function hidePopup(pid) {
-        /*Colorize Customization button if textbox contains content*/
-        let textbox = document.getElementById("comment-" + pid);
-        let customizeButton = document.getElementById("customize-button-" + pid);
+        //Sync second amount field
+        amounts2[i].innerHTML = amount;
 
-        if (textbox.value === "") {
-            customizeButton.style.backgroundColor = null;
+        //Colorize table row if amount is > 0
+        if (parseFloat(amount) > 0) {
+            rows[i].classList.add("colorized-row");
+
+            //Generate overview
+            let entry = document.createElement('li');
+            let entry2 = document.createElement('li');
+
+            let entry_text = amount + "x " + name
+            entry_text += (comment !== "" ? " (" + comment + ")" : "");
+            let entry_text2 = current_cost.toFixed(2).toString() + "€";
+
+            entry.appendChild(document.createTextNode(entry_text));
+            entry2.appendChild(document.createTextNode(entry_text2));
+
+            overview.appendChild(entry);
+            overview.appendChild(entry2);
         }
         else {
-            customizeButton.style.backgroundColor = "#4CAF50";
+            rows[i].classList.remove("colorized-row");
         }
-
-        /*Hide popup*/
-        document.getElementById("customize-popup-" + pid).style.display = "none";
-
-        updateValues();
     }
 
-    function modifyAmount(pid, value) {
-        //Modify current value
-        let textbox = document.getElementById("amount-" + pid);
-        let current_value = parseInt(textbox.value);
-        let new_value;
+    //Recompute total value
+    let total = document.getElementById("total-cost");
+    total.innerHTML = sum.toFixed(2).toString();
+}
 
-        if (isNaN(current_value)) {
-            new_value = 0;
-        } else {
-            new_value = current_value + value;
-        }
+function showPopup(pid) {
+    /*Display popup*/
+    document.getElementById("customize-popup-" + pid).style.display = "block";
+}
 
-        if (new_value <= 0) {
-            new_value = 0;
-        }
+function hidePopup(pid) {
+    /*Colorize Customization button if textbox contains content*/
+    let textbox = document.getElementById("comment-" + pid);
+    let customizeButton = document.getElementById("customize-button-" + pid);
 
-        textbox.value = new_value;
-        updateValues();
+    if (textbox.value === "") {
+        customizeButton.style.backgroundColor = null;
+    }
+    else {
+        customizeButton.style.backgroundColor = "#4CAF50";
     }
 
-    function toggleCategoryFold(pcat) {
-        let cfd = document.getElementById("category-fold-div-" + pcat);
-        let cfb = document.getElementById("category-button-" + pcat);
+    /*Hide popup*/
+    document.getElementById("customize-popup-" + pid).style.display = "none";
 
-        if (! cfd.classList.contains("hidden")) {
-            //Element is shown and will be hidden
-            //Set max height for later unfold
-            cfd.classList.remove("category-fold-div");  //disable transitions for this step
-            cfd.style.maxHeight = cfd.offsetHeight + "px";
-            cfd.classList.add("category-fold-div");  //reenable transitions
-        }
+    updateValues();
+}
 
-        ["transitionEnd", "webkitTransitionEnd", "transitionend", "oTransitionEnd", "msTransitionEnd"].forEach(function(e) {
-            cfd.addEventListener(e, function() {
-                if(! cfd.classList.contains("hidden")){
-                    //Element was hidden and is shown, transition is over
-                    cfd.style.maxHeight = "";  //Reset max-height to allow element to resize to auto
-                }
-            });
+function modifyAmount(pid, value) {
+    //Modify current value
+    let textbox = document.getElementById("amount-" + pid);
+    let current_value = parseInt(textbox.value);
+    let new_value;
+
+    if (isNaN(current_value)) {
+        new_value = 0;
+    } else {
+        new_value = current_value + value;
+    }
+
+    if (new_value <= 0) {
+        new_value = 0;
+    }
+
+    textbox.value = new_value;
+    updateValues();
+}
+
+function toggleCategoryFold(pcat) {
+    let cfd = document.getElementById("category-fold-div-" + pcat);
+    let cfb = document.getElementById("category-button-" + pcat);
+
+    if (! cfd.classList.contains("hidden")) {
+        //Element is shown and will be hidden
+        //Set max height for later unfold
+        cfd.classList.remove("category-fold-div");  //disable transitions for this step
+        cfd.style.maxHeight = cfd.offsetHeight + "px";
+        cfd.classList.add("category-fold-div");  //reenable transitions
+    }
+
+    ["transitionEnd", "webkitTransitionEnd", "transitionend", "oTransitionEnd", "msTransitionEnd"].forEach(function(e) {
+        cfd.addEventListener(e, function() {
+            if(! cfd.classList.contains("hidden")){
+                //Element was hidden and is shown, transition is over
+                cfd.style.maxHeight = "";  //Reset max-height to allow element to resize to auto
+            }
         });
+    });
 
-        //Set a small timeout (1ms probably also works but we use 10ms to be sure)
-        //If the reenable transitions step comes too quick before a event that would enable
-        //transitions, the transition will not fire. With the timeout the browser has enough
-        //time to enable the transition
-        setTimeout(function(){
-            cfd.classList.toggle("hidden");
-            cfb.classList.toggle("arrowturn");
-        }, 10);
+    //Set a small timeout (1ms probably also works but we use 10ms to be sure)
+    //If the reenable transitions step comes too quick before a event that would enable
+    //transitions, the transition will not fire. With the timeout the browser has enough
+    //time to enable the transition
+    setTimeout(function(){
+        cfd.classList.toggle("hidden");
+        cfb.classList.toggle("arrowturn");
+    }, 10);
+}
+
+function initialCategoryFold() {
+    let divs = document.getElementsByClassName("category-fold-div");
+    let buttons = document.getElementsByClassName("category-button");
+
+    //Set height and hide by default
+    for(let i=0; i<divs.length; i++) {
+        divs[i].style.maxHeight = divs[i].offsetHeight + "px";
+        divs[i].classList.toggle("hidden");
+    }
+    //Turn arrows to correct position
+    for(let i=0; i<buttons.length; i++) {
+        buttons[i].classList.toggle("arrowturn");
+    }
+}
+
+/*
+ * Service Overview
+ */
+
+function updateValues2() {
+    let classes = [
+        "amount-box",
+        "product-name",
+        "product-price",
+        "product-row"
+    ];
+
+    let clselements= classes.map(cls => Array.from(document.getElementsByClassName(cls)));
+    clselements.forEach((cls) => cls.sort(compare_ids));
+    let [amounts, names, prices, rows] = clselements;
+
+    let sum = 0;
+    let overview = document.getElementById("overview");
+
+    overview.innerHTML = "";  //clear children of overview list
+
+    for(let i=0; i<prices.length; i++) {
+        //Catch negative values
+        if (amounts[i].value < 0) {
+            amounts[i].value = 0;
+        }
+
+        //Extract values
+        let amount = amounts[i].value;
+        let name = names[i].innerHTML;
+        let price = prices[i].innerHTML;
+
+        //Compute accumulation
+        let current_cost = parseFloat(price) * parseFloat(amount);
+        sum += current_cost;
+
+        //Colorize table row if amount is > 0
+        if (parseFloat(amount) > 0) {
+            //rows[i].classList.add("colorized-row");  TODO
+
+            //Generate overview
+            let entry = document.createElement('li');
+            let entry2 = document.createElement('li');
+
+            let entry_text = amount + "x " + name;
+            let entry_text2 = current_cost.toFixed(2).toString() + "€";
+
+            entry.appendChild(document.createTextNode(entry_text));
+            entry2.appendChild(document.createTextNode(entry_text2));
+            overview.appendChild(entry);
+            overview.appendChild(entry2);
+        }
+        else {
+            //rows[i].classList.remove("colorized-row");  TODO
+        }
     }
 
-    function initialCategoryFold() {
-        let divs = document.getElementsByClassName("category-fold-div");
-        let buttons = document.getElementsByClassName("category-button");
+    //Recompute total value
+    let total = document.getElementById("total-cost");
+    total.innerHTML = sum.toFixed(2).toString();
+}
 
-        //Set height and hide by default
-        for(let i=0; i<divs.length; i++) {
-            divs[i].style.maxHeight = divs[i].offsetHeight + "px";
-            divs[i].classList.toggle("hidden");
-        }
-        //Turn arrows to correct position
-        for(let i=0; i<buttons.length; i++) {
-            buttons[i].classList.toggle("arrowturn");
-        }
+function modifyAmount2(pid, value) {
+    //Modify current value
+    let textbox = document.getElementById("amount-" + pid);
+    let span = document.getElementById("max-amount-" + pid);
+
+    let current_value = parseInt(textbox.value);
+    let max_value = parseInt(span.innerHTML);
+    let new_value;
+
+    if (isNaN(current_value)) {
+        new_value = 0;
+    } else if (current_value + value > max_value) {
+        new_value = current_value;
+    } else {
+        new_value = current_value + value;
     }
 
-    /*
-     * Service Overview
-     */
-
-    function updateValues2() {
-        let classes = [
-            "amount-box",
-            "product-name",
-            "product-price",
-            "product-row"
-        ];
-
-        let clselements= classes.map(cls => Array.from(document.getElementsByClassName(cls)));
-        clselements.forEach((cls) => cls.sort(compare_ids));
-        let [amounts, names, prices, rows] = clselements;
-
-        let sum = 0;
-        let overview = document.getElementById("overview");
-
-        overview.innerHTML = "";  //clear children of overview list
-
-        for(let i=0; i<prices.length; i++) {
-            //Catch negative values
-            if (amounts[i].value < 0) {
-                amounts[i].value = 0;
-            }
-
-            //Extract values
-            let amount = amounts[i].value;
-            let name = names[i].innerHTML;
-            let price = prices[i].innerHTML;
-
-            //Compute accumulation
-            let current_cost = parseFloat(price) * parseFloat(amount);
-            sum += current_cost;
-
-            //Colorize table row if amount is > 0
-            if (parseFloat(amount) > 0) {
-                //rows[i].classList.add("colorized-row");  TODO
-
-                //Generate overview
-                let entry = document.createElement('li');
-                let entry2 = document.createElement('li');
-
-                let entry_text = amount + "x " + name
-                let entry_text2 = current_cost.toFixed(2).toString() + "€"
-
-                entry.appendChild(document.createTextNode(entry_text));
-                entry2.appendChild(document.createTextNode(entry_text2));
-                overview.appendChild(entry);
-                overview.appendChild(entry2);
-            }
-            else {
-                //rows[i].classList.remove("colorized-row");  TODO
-            }
-        }
-
-        //Recompute total value
-        let total = document.getElementById("total-cost");
-        total.innerHTML = sum.toFixed(2).toString();
+    if (new_value <= 0) {
+        new_value = 0;
     }
 
-    function modifyAmount2(pid, value) {
-        //Modify current value
-        let textbox = document.getElementById("amount-" + pid);
-        let span = document.getElementById("max-amount-" + pid);
+    textbox.value = new_value;
+    updateValues2();
+}
 
-        let current_value = parseInt(textbox.value);
-        let max_value = parseInt(span.innerHTML);
-        let new_value;
+function payPartially() {
+    let classes = [
+        "amount-box",
+        "max-amount",
+        "product-row"
+    ];
 
-        if (isNaN(current_value)) {
-            new_value = 0;
-        } else if (current_value + value > max_value) {
-            new_value = current_value;
-        } else {
-            new_value = current_value + value;
-        }
+    let clselements= classes.map(cls => Array.from(document.getElementsByClassName(cls)));
+    clselements.forEach((cls) => cls.sort(compare_ids));
+    let [amounts, max_amounts, rows] = clselements;
 
-        if (new_value <= 0) {
-            new_value = 0;
-        }
-
-        textbox.value = new_value;
-        updateValues2();
-    }
-
-    function payPartially() {
-        let classes = [
-            "amount-box",
-            "max-amount",
-            "product-row"
-        ];
-
-        let clselements= classes.map(cls => Array.from(document.getElementsByClassName(cls)));
-        clselements.forEach((cls) => cls.sort(compare_ids));
-        let [amounts, max_amounts, rows] = clselements;
-
-        for(let i=0; i<rows.length; i++) {
-            //Extract values
-            let amount = amounts[i].value;
-            let max_amount = max_amounts[i].innerHTML;
+    for(let i=0; i<rows.length; i++) {
+        //Extract values
+        let amount = amounts[i].value;
+        let max_amount = max_amounts[i].innerHTML;
 
         if (parseFloat(amount) == parseFloat(max_amount)) {
             //Remove line
